054f25b2e28b334f94b67c7878fc74333bfd60c1
paulnguyendesigns <paulxnguyen12@gmail.com>
2026-08-08 13:20:21 -0700

    Added CodeMonkey Assets

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour {
+
+    void Start() {
+        Debug.Log("GameHandler.Start");
+
+        GameObject snakeHeadGameObject = new GameObject();
+        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRenderer.sprite = GameAssets.i.snakeHeadSprite;
+    }
+
+    void Update() {
+        
+    } 
+}
---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour {
+    private Vector2Int gridMoveDirection;
+    private Vector2Int gridPosition;
+    private float gridMoveTimer;
+    private float gridMoveTimerMax;
+
+    private void Awake() {
+        gridPosition = new Vector2Int(0,0);
+        gridMoveTimerMax = 0.2f;
+        gridMoveTimer = gridMoveTimerMax;
+        gridMoveDirection = new Vector2Int(1,0);
+
+        UpdateSnakeRotation();
+    }
+
+    private void Update() {
+        HandleInput();
+        HandleGridMovement();
+    }
+    private void UpdateSnakeRotation() {
+        float angle = Mathf.Atan2(gridMoveDirection.y, gridMoveDirection.x) * Mathf.Rad2Deg + 90f;
+        transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private void HandleInput() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            if (gridMoveDirection.y != -1) {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = 1;                
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            if (gridMoveDirection.y != 1) {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = -1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            if (gridMoveDirection.x != 1) {
+                gridMoveDirection.x = -1;
+                gridMoveDirection.y = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            if (gridMoveDirection.x != -1) {
+                gridMoveDirection.x = 1;
+                gridMoveDirection.y = 0;
+            }
+        }
+    }
+    private void HandleGridMovement() {
+        gridMoveTimer += Time.deltaTime;
+        if (gridMoveTimer >= gridMoveTimerMax) {
+            gridPosition += gridMoveDirection;
+            gridMoveTimer -= gridMoveTimerMax;
+
+            UpdateSnakeRotation();
+        }
+        transform.position = new Vector3(gridPosition.x, gridPosition.y);
+    }
+}
+
